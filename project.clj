d87f3309d7bef6e34962afb37c134066000709ef
tempxla <38803749+tempxla@users.noreply.github.com>
2021-12-29 01:52:13 +0900

    基本部品を準備

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [manifold "0.2.3"]]
+                 [manifold "0.2.3"]
+                 [aleph "0.4.6"]]
   :repl-options {:init-ns aleph-tcp-sample.core})
--- src/aleph_tcp_sample/core.clj
+++ src/aleph_tcp_sample/core.clj
@@ -1,6 +1,32 @@
-(ns aleph-tcp-sample.core)
+(ns aleph-tcp-sample.core
+  (:require [aleph.tcp :as tcp]
+            [manifold.deferred :as d]
+            [manifold.stream :as s]
+            [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def echo-server (atom nil))
+
+(defn response-echo [^String x]
+  (str/join "\r\n"
+            ["HTTP/1.1 200 OK"
+             "Server: echo-server"
+             (str "Date: " (.format java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME
+                                    (java.time.OffsetDateTime/now java.time.ZoneOffset/UTC)))
+             "Content-Type: text/plain"
+             (str "Content-Length: " (.length x))
+             ""
+             x]))
+
+(defn echo-handler [s info]
+  (s/connect s s))
+
+(defn go []
+  (reset! echo-server (tcp/start-server echo-handler {:port 10001})))
+
+(defn halt []
+  (when @echo-server
+    (.close @echo-server)))
+
+(defn reset []
+  (halt)
+  (go))
